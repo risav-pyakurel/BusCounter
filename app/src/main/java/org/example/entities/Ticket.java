9addf99ed479624cad4e94ac5a4984965ca90eb1
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-29 23:32:24 +0545

    Created new entities

---
+++ app/src/main/java/org/example/entities/Ticket.java
@@ -0,0 +1,13 @@
+package org.example.entities;
+
+import java.util.Date;
+
+public class Ticket {
+    private String TicketId;
+    private String UserId;
+    private String Source;
+    private String Destination;
+    private Date dateofTravel;
+    private Bus bus;
+
+}
---
+++ app/src/main/java/org/example/entities/User.java
@@ -0,0 +1,14 @@
+package org.example.entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashPassword;
+    private List<Ticket> ticketsBooked;
+    private String userId;
+    private String ContactNumber;
+
+
+}
