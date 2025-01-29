8f30935ee5e39f3a44f57ba4b6795b5b851216c6
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-29 23:45:46 +0545

    Created Services

--- app/src/main/java/org/example/entities/Bus.java
+++ app/src/main/java/org/example/entities/Bus.java
@@ -1,4 +1,13 @@
 package org.example.entities;
 
+import java.sql.Time;
+import java.util.List;
+import java.util.Map;
+
 public class Bus {
+    private String busId;
+    private String busNumber;
+    private List<List<Integer>> seats;
+    private Map<String, Time> stationTimes;
+    private List<String> stations;
 }
