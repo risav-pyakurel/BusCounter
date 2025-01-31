68c8ce966596feae22fe09899158fc9eb64a9328
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-31 15:38:26 +0545

    deserializing the JSON

--- app/src/main/java/org/example/Service/UserBookingService.java
+++ app/src/main/java/org/example/Service/UserBookingService.java
@@ -1,13 +1,29 @@
 package org.example.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entities.User;
 
-public class UserBookingService {
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class UserBookingService
+{
 
     private User user;
 
-    public UserBookingService(User user1){
+    private List<User> userList;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String USERS_PATH = "../LocalDB/users.json";
+
+    public UserBookingService(User user1)  throws IOException
+    {
         this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>(){});
     }
 
 }
