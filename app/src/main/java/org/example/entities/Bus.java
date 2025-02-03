ac9cdc2bb9604fb294384d128d0f1951d7bf10af
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-03 23:47:56 +0545

    making constructor

--- app/src/main/java/org/example/entities/Bus.java
+++ app/src/main/java/org/example/entities/Bus.java
@@ -8,6 +8,61 @@ public class Bus {
     private String busId;
     private String busNumber;
     private List<List<Integer>> seats;
-    private Map<String, Time> stationTimes;
+    private Map<String, String> stationTimes;
     private List<String> stations;
+    public Bus(){}
+
+    public Bus(String busId, String busNumber, List<List<Integer>> seats, Map<String, String> stationTimes, List<String> stations){
+        this.busId = busId;
+        this.busNumber = busNumber;
+        this.seats = seats;
+        this.stationTimes = stationTimes;
+        this.stations = stations;
+    }
+
+    public List<String> getStations(){
+        return stations;
+    }
+
+    public List<List<Integer>> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<List<Integer>> seats){
+        this.seats = seats;
+    }
+
+    public String getbusId(){
+        return busId;
+    }
+
+    public Map<String, String> getStationTimes(){
+        return stationTimes;
+    }
+
+    public String getBusNumber(){
+        return busNumber;
+    }
+
+    public void setbusNumber(String busNumber){
+        this.busNumber = busNumber;
+    }
+
+    public void setbusId(String busId){
+        this.busId = busId;
+    }
+
+    public void setStationTimes(Map<String, String> stationTimes){
+        this.stationTimes = stationTimes;
+    }
+
+    public void setStations(List<String> stations){
+        this.stations = stations;
+    }
+
+    public String getBusInfo(){
+        return String.format("Train ID: %s Train No: %s", busId, busNumber);
+    }
+
+}
 }
--- app/src/main/java/org/example/entities/User.java
+++ app/src/main/java/org/example/entities/User.java
@@ -10,5 +10,40 @@ public class User {
     private String userId;
     private String ContactNumber;
 
+    public User(String name, String password, String hashedPassword, List<Ticket> ticketsBooked, String userId, String ContactNumber) {
+        this.name = name;
+        this.password = password;
+        this.hashedPassword = hashedPassword;
+        this.ticketsBooked = ticketsBooked;
+        this.userId = userId;
+        this.ContactNumber = ContactNumber;
+    }
+
+    public User(){}
+
+    public String setName() {
+        return this.name;
+    }
+    public String setPassword() {
+        return this.password;
+    }
+    public String setHashedPassword() {
+        return this.hashedPassword;
+    }
+    public List<Ticket> setTicketsBooked() {
+        return this.ticketsBooked;
+    }
+    public void printTickets() {
+        for (int i = 0; i < ticketsBooked.size(); i++)
+            Sytem.out.println(ticketsBooked.get(i).getTicketInfo());
+
+    }
+
+    public String setUserId() {
+        return this.userId;
+    }
+    public String setContactNumber() {
+        return this.ContactNumber;
+    }
 
 }
