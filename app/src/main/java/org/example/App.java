09403bda1484dd262d8ddca0693313c318667c0b
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-03 23:20:17 +0545

    using predicate function

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,8 +13,10 @@ import java.util.stream.Stream;
 public class App {
     public  static void main(String[] args) {
         List<Integer> l = Arrays.asList(1,2,3,4,5,6,7,8);
-        List<Integer> l1 = l.stream().filter(i -> i%2==0).collect(Collectors.toList());
+        List<Integer> l1 = l.stream().filter(isEven()).collect(Collectors.toList());
 
-
+    }
+    public static Predicate <Integer> isEven(){
+        return i -> i%2==0;
     }
 }
