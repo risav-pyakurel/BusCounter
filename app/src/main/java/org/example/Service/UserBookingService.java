9bfda077d82e199570f868f43246227d25be8df4
risav-pyakurel <pyakurel.risav@gmail.com>
2025-02-07 22:02:57 +0545

    deserializing the json file

--- app/src/main/java/org/example/Service/UserBookingService.java
+++ app/src/main/java/org/example/Service/UserBookingService.java
@@ -30,10 +30,25 @@ public class UserBookingService
     public Boolean loginUser(){
         Optional <User> foundUser = userList.stream().filter(user1 ->
         {
-            return user1.getName().equalsIgnoreCase(user.getName()) && UserServiceUtil.CheckPassword(user.getPassword(), user1.getHashedPassword())
+            return user1.getName().equalsIgnoreCase(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
         }).findFirst();
 
 
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return  Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+    private void saveUserListToFile() throws IOException {
+        File usersFile = new File(USERS_PATH);
+        objectMapper.writeValue(usersFile, userList);
     }
 
 }
